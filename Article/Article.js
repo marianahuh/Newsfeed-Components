83e753d288d7194973fed5b9b53302ec1b29f90e
Mariana <mhuh0176@gmail.com>
2019-02-05 21:09:29 -0800

    Completed Newsfeed-Components

--- Article/Article.js
+++ Article/Article.js
@@ -2,12 +2,14 @@
 
 class Article {
   constructor(article) {
+    this.article = article;
     // assign this.domElement to the passed in domElement
     this.articleOpen = article.querySelector('.article-open');
     this.articleClose = article.querySelector('.close');
     // create a reference to the ".expandButton" class.
     this.expandButton = article.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton.textContent = 'Expand';
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
@@ -16,8 +18,8 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     console.log('expand');
-    this.articleOpen.classList.toggle('.article-open');
-    this.articleClose.classList.toggle('.close');
+    this.article.classList.toggle('article-open');
+
   }
 }
 
@@ -30,5 +32,5 @@ class Article {
 
 */
 
-let articles = document.querySelectorAll('.articles');
+let articles = document.querySelectorAll('.article');
 articles.forEach(article => new Article(article));
